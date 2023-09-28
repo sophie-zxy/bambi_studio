5588ec7146a42ea0fb1d7d4902deda18c6622235
Sophie <lucyxinyuzhang@gmail.com>
2023-09-28 19:00:23 -0400

    photography page - this is a bad solution

---
+++ src/lib/ImageCard.svelte
@@ -0,0 +1,9 @@
+<script>
+    // @ts-ignore
+    export let src = '';
+    export let alt = '';
+</script>
+
+<div class="image-card">
+    <img class="h-auto max-w-full mb-4" {src} {alt}>
+</div>
--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -1,14 +1,56 @@
-<script>
+<script lang="ts">
+
     import LeftAndRightArrow from "$lib/LeftAndRightArrow.svelte";
+    import ImageCard from "$lib/ImageCard.svelte";
+
     let nav_obj = {
         left_link_txt: "Illustration",
         left_link: "/illustration",
         right_link_txt: "Sketches",
         right_link: "/sketches_route"
     }
+
+    let images = [
+        {src: "../../photo_page_pics/photo1.JPG", alt: "cherry blossom"},
+        {src: "../../photo_page_pics/photo2.JPG", alt: "bench"},
+        // {src: "../../photo_page_pics/photo1.JPG", alt: "cherry blossom"},
+        // {src: "../../photo_page_pics/photo2.JPG", alt: "bench"},
+        // {src: "../../photo_page_pics/photo2.JPG", alt: "bench"},
+        // {src: "../../photo_page_pics/photo1.JPG", alt: "cherry blossom"},
+        // {src: "../../photo_page_pics/photo1.JPG", alt: "cherry blossom"},
+        // {src: "../../photo_page_pics/photo2.JPG", alt: "bench"},
+        // {src: "../../photo_page_pics/photo2.JPG", alt: "bench"},
+        // {src: "../../photo_page_pics/photo1.JPG", alt: "cherry blossom"},
+    ]
+
+    // make image group 1 to contain only odd entries of images in images array
+    let image_group1: {src: string, alt: string}[] = [];
+    let image_group2: {src: string, alt: string}[] = [];
+
+
+    for (let i = 0; i < images.length; i++) {
+        if (i % 2 == 0) {
+            image_group1.push(images[i])
+        } else {
+            image_group2.push(images[i])
+        }
+    }
 </script>
 
-<html lang="postcss">
-    <h1>This page contains content about bambi's photography stuff</h1>
-    <LeftAndRightArrow {...nav_obj}/>
-</html>
+<style>
+</style>
+
+<div class="grid grid-cols-2 gap-5 m-14">
+    <div>
+        {#each image_group1 as image}
+            <ImageCard {...image}/>
+        {/each}
+    </div>
+    <div>
+        {#each image_group2 as image}
+            <ImageCard {...image}/>
+        {/each}
+    </div>
+</div>
+
+<LeftAndRightArrow {...nav_obj}/>
