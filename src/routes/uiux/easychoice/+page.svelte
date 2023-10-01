45a8ef0fbac900d3ca03ef332037d996742d3045
Sophie <lucyxinyuzhang@gmail.com>
2023-10-01 17:18:09 -0400

    wip, making the page responsive

--- src/routes/uiux/easychoice/+page.svelte
+++ src/routes/uiux/easychoice/+page.svelte
@@ -1,3 +1,85 @@
-<html lang="postcss">
-    <h1>This is the easy choice page</h1>
-</html>
+
+<div class="backdrop">
+    <div class="hero min-h-screen">
+        <div class="hero-content text-center text-neutral-content">
+            <div class="grid grid-cols-2">
+            <div>
+                <img src="../../../uiux_easychoice_page_pics/easychoice translucent.png" alt="ec">
+            </div>
+            <div class="flex items-center">
+                <div class="grid justify-items-start mx-16">
+                    <p class="mb-5">Easychoice</p>
+                    <h1 class="mb-5 text-4xl text-left">A web that helps people with selection difficulties.</h1>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="mx-10 lg:mx-24 mt-16">
+        <h1 class="text-heading-group text-5xl text-center mb-12">Research</h1>
+        <p class="text-md ml-4 lg:ml-8 mb-8">Nowadays, we are faced with countless choices every day, and there are many people who have a kind of mild or severe selection difficulties. </p>
+        <p class="text-md mx-8 lg:mx-20 leading-9">“Making decisions can be a terrifying prospect to many people who are afraid of what will happen if they don’t choose the right thing. The fear of making the wrong decision, or “decidophia” - a term coined by Princeton University philosopher Walter Kaufmann inhis book Without Guilt and Justice - can effect people wven when it comes to the smallest choices, such as what to have lunch or what to wear. Decidophobia can lead to blurred thinking, a lack of clarity and increased dependence on others to choose for you, all leading to sn overall lost sense of direction and control. "</p>
+        <p class="text-md text-right">—— Jul 5, 2017, Forbes Coaches Council</p>
+
+        <h1 class="text-heading-group text-5xl text-center mb-12 tracking-wider mt-16">Chart of The Propotion of College Students With or Without Selection Difficulties</h1>
+        <p class="text-md text-right">—— Data from CYN Education</p>
+
+        <h1 class="text-heading-group text-5xl text-center mb-12 tracking-wider mt-16">Chart of The Propotion of Aspects in Which College Students Have Difficulty in Making Choices</h1>
+        <p class="text-md text-right">—— Data from CYN Education</p>
+
+        <h1 class="text-heading-group text-5xl text-center mb-12 mt-16">Solution</h1>
+        <p class="text-md ml-8 mb-8">Life is often faced with many choices, no matter big or small, in front of the choice, what will you do, whether you are decisive and sure or hesitant, in fact, it is possible. That's why I designed Easychoice to help you make a faster and more decisive choice. </p>
+        <p class="text-md ml-8 mb-8">As the saying goes, the moment the coin is tossed, you already know the answer. Easychoice can help you clarify your inner thoughts, correctly understand what you want, learn to trust and appreciate yourself, increase the self-confidence of choice, will find that the choice is not a difficult thing!</p>
+        
+        <div class="image-group">
+            <h1 class="text-5xl mb-12">User Persona</h1>
+            <img src="../../../uiux_easychoice_page_pics/persona2.png" alt="persona2">
+            <img src="../../../uiux_easychoice_page_pics/persona1.png" alt="persona1">
+            
+            <h1 class="text-5xl mb-12">User Flow</h1>
+            <img src="../../../uiux_easychoice_page_pics/userflow.png" alt="userflow">
+            
+            <h1 class="text-5xl my-12">Design Element</h1>
+            <img src="../../../uiux_easychoice_page_pics/easychoice designelement.png" alt="element">
+            
+            <h1 class="text-5xl my-12">Wireframe</h1>
+            <img src="../../../uiux_easychoice_page_pics/wireframe.png" alt="wireframe">
+            
+            <h1 class="text-5xl my-12">Visual Design</h1>
+            <img src="../../../uiux_easychoice_page_pics/frame1.png" alt="frame1">
+            <img src="../../../uiux_easychoice_page_pics/frame3.png" alt="frame3">
+            <img src="../../../uiux_easychoice_page_pics/frame2.png" alt="frame2">
+            <img src="../../../uiux_easychoice_page_pics/frame4.png" alt="frame4">
+        </div>
+    </div>
+<!-- 
+    <div class="mx-20 mt-16">
+        <h1 class="text-heading-group text-5xl text-center mb-12 tracking-wider">Chart of The Propotion of College Students With or Without Selection Difficulties</h1>
+        <p class="text-md text-right">—— Data from CYN Education</p>
+    </div>
+
+    <div class="mx-20 mt-16">
+        <h1 class="text-heading-group text-5xl text-center mb-12 tracking-wider">Chart of The Propotion of Aspects in Which College Students Have Difficulty in Making Choices</h1>
+        <p class="text-md text-right">—— Data from CYN Education</p>
+    </div>
+
+    <div class="mx-20 mt-16">
+        <h1 class="text-heading-group text-5xl text-center mb-12">Solution</h1>
+        <p class="text-md ml-8 mb-8">Life is often faced with many choices, no matter big or small, in front of the choice, what will you do, whether you are decisive and sure or hesitant, in fact, it is possible. That's why I designed Easychoice to help you make a faster and more decisive choice. </p>
+        <p class="text-md ml-8 mb-8">As the saying goes, the moment the coin is tossed, you already know the answer. Easychoice can help you clarify your inner thoughts, correctly understand what you want, learn to trust and appreciate yourself, increase the self-confidence of choice, will find that the choice is not a difficult thing!</p>
+    </div> -->
+</div>
+
+<style>
+    .backdrop {
+        background-color: rgba(230,228,232,255)
+    }
+
+    .hero {
+        background-color: rgba(98,84,72,255);
+    }
+
+    .text-heading-group {
+        color: rgba(199,146,136,255)
+    }
+</style>
