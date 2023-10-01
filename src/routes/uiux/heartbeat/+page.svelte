cc0d81b88137364f1431355db8108afadf98d75f
Sophie <lucyxinyuzhang@gmail.com>
2023-10-01 14:43:37 -0400

    heartbeat page completion

--- src/routes/uiux/heartbeat/+page.svelte
+++ src/routes/uiux/heartbeat/+page.svelte
@@ -1,3 +1,99 @@
 <html lang="postcss">
-    <h1>This is the heartbeat page</h1>
+    <div class="hero min-h-screen" style="background-image: url('../../../uiux_heartbeat_page_pics/Hb.png');">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="text-neutral-content">
+          <div class="mr-12 md:mr-40 lg:mr-52 xl:mr-auto ml-12 max-w-3xl">
+            <h1 class="mb-5 text-5xl">Introduction</h1>
+            <p class="mb-5 text-xl">This is a music software that can search for nearby users who listen to the same song as you, and you can choose to chat or not.</p>
+            <p class="mb-5 text-xl">Through different music types and works, find people with the same taste to become friends.</p>
+        </div>
+    </div>
+</div>
+<video class="videoContainer w-full" src="../../../uiux_heartbeat_page_pics/App_demoreel.mp4" autoplay loop muted></video>
+<div class="grid justify-items-center mt-40">
+    <h1 class="text-heading mb-5 text-4xl">Research: User Interview</h1>
+    <p class="my-3 text-md">Do you like music?</p>
+    <p class="my-3 text-md">Are you shy to post yourself on social media?</p>
+    <p class="my-3 text-md">Are you afraid to communicate with people?</p>
+    <p class="my-3 text-md">Are you used to listening to music by yourself?</p>
+</div>
+<div class="mx-12">
+    <div class="grid justify-items-center">
+        <div class="grid justify-items-start">
+            <h1 class="text-heading my-7 text-4xl">Problem</h1>
+            <ul class="pl-5">
+                <li>Most of the current social media are photo social media</li>
+                <li>People who are not confident about their looks or body may not be able to use this kind of software</li>
+                <li>People who do not like to post their photos also do not suit this kind of social media</li>
+                <li>Some people will steal other people's photos</li>
+            </ul>
+    
+            <h1 class="text-heading my-7 text-4xl">Solution</h1>
+            <p class="pl-5 pb-7">There is a music software that can search for nearby users who listen to the same song as you. And you can choose to chat with them or not.</p>
+            <ul class="pl-5">
+                <li>Through different music types and works, find people with the same taste to become friends</li>
+                <li>Help those who are not good at expressing and communicating</li>
+                <li>Not only look at people’s appearance, no need to define others by appearance</li>
+            </ul>
+        </div>
+    </div>
+    <div class="grid justify-items-center">
+        <div class="max-w-6xl">
+            <div class="mb-20">
+                <h1 class="pic-heading mb-7 mt-16 text-4xl">User Personas</h1>
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/heartbeat1.png" alt="heartbeat1">
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/heartbeat2.png" alt="heartbeat2">
+            </div>
+            <div class="mb-20">
+                <h1 class="pic-heading mb-7 mt-16 text-4xl">User Flow</h1>
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/hb flow.png" alt="flow">
+            </div>
+            <div class="mb-20">
+                <h1 class="pic-heading mb-7 mt-16 text-4xl">User Journey</h1>
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/userjourney.png" alt="userjourney">
+            </div>
+            <div class="mb-20">
+                <h1 class="pic-heading mb-7 mt-16 text-4xl">Design Element</h1>
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/HB Design Element.png" alt="element">
+            </div>
+            <div class="mb-20">
+                <h1 class="pic-heading mb-7 mt-16 text-4xl">Wireframe</h1>
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/wireframe.png" alt="Wireframe">
+            </div>
+            <div class="mb-20">
+                <h1 class="pic-heading mb-7 mt-16 text-4xl">Visual Design</h1>
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/music page.png" alt="Visual">
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/Frame 10.png" alt="Visual1">
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/Frame 11.png" alt="Visual2">
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/Frame 12.png" alt="Visual3">
+                <img class="img-group" src="../../../uiux_heartbeat_page_pics/Frame 13.png" alt="Visual4">
+            </div>
+        </div>
+    </div>
+</div>
 </html>
+
+<style>
+    html {
+        font-family: 'EB Garamond';
+        background-color: black;
+    }
+    .text-heading {
+        color: rgba(199,146,136,255);
+    }
+    p, li {
+        color: white
+    }
+    .pic-heading {
+        color: white;
+    }
+    /* make the li tag bullet point style */
+    li {
+        list-style-type: disc;
+        list-style-position: inside;
+        /* add more spacing between each list element */
+        margin-bottom: 15px;
+        font-size: medium;
+        font-weight: 100;
+    }
+</style>
