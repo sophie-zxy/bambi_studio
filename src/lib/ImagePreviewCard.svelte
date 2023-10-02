b2fbdad2a9b876efac6adba1e59473863bccfad6
Sophie <lucyxinyuzhang@gmail.com>
2023-10-02 01:18:21 -0400

    make the home page prettier

--- src/lib/ImagePreviewCard.svelte
+++ src/lib/ImagePreviewCard.svelte
@@ -14,29 +14,22 @@
 </script>
 
 <a href={link}>
-    <div class="card only:bg-cover bg-center"
-     style="background-image: url({src}); background-size: cover; width: {squareDimension}; height: {squareDimension}; filter: brightness(.8);"
-     role="button" 
-     tabindex="0">
-     <p class="text p-4 text-white">{text}</p>
+    <div class="card">
+        <div class="card only:bg-cover bg-center"
+         style="background-image: url({src}); background-size: cover; width: {squareDimension}; height: {squareDimension}; filter: brightness(0.7)">
+        </div>
+        <p class="text p-4 text-white">{text}</p>
     </div>
 </a>
 
 <style>
     .card {
         position: relative;
-        color: rgba(255, 255, 255, 0.2);
     }
-    /* .card:hover {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        transition: opacity 3s;
-        opacity: 1;
-    } */
+    .card:hover {
+        transform: scale(1.02);
+        transition-duration: 200ms;
+    }
     .text {
         font-family: "EB Garamond";
         font-weight: 400;
@@ -45,8 +38,7 @@
         left: 50%;
         transform: translate(-50%, -50%);
         font-size: 32px;
-        font-weight: 200;
-        color: black;
         text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+        z-index: 5;
     }
 </style>
