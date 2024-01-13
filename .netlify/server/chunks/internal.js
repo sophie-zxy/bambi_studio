81f3f5890a14240b50900cbeaa7015727b70d2f7
bambi <68458273+lzy973743411@users.noreply.github.com>
2024-01-12 23:32:48 -0500

    change 1
    change 1

--- .netlify/server/chunks/internal.js
+++ .netlify/server/chunks/internal.js
@@ -195,7 +195,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "153pz2u"
+  version_hash: "4ppe1c"
 };
 function get_hooks() {
   return {};
