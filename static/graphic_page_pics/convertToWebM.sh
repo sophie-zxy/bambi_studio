08537963a11d2b17109bf819fc83d465c811be2c
Sophie <lucyxinyuzhang@gmail.com>
2024-01-28 14:42:26 -0500

    Update convertToWebM.sh

--- static/graphic_page_pics/convertToWebM.sh
+++ static/graphic_page_pics/convertToWebM.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-for file in *.jpg; do
-  base=$(basename "$file" .jpg)
-  ffmpeg -i "$file" "${base}.webp"
+# chmod +x convert_to_webp.sh
+
+# Array of extensions
+extensions=("png" "jpg" "jpeg" "gif")
+
+# Loop over each extension
+for ext in "${extensions[@]}"; do
+    # Loop over each file with the current extension
+    for file in *."$ext"; do
+        # Check if the file exists to avoid errors
+        if [[ -f "$file" ]]; then
+            base=$(basename "$file" ".$ext")
+            ffmpeg -i "$file" "${base}.webp"
+        fi
+    done
 done
