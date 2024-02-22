103c12786e3824fbf77017cc34ec8daacb58a609
bambi <68458273+lzy973743411@users.noreply.github.com>
2024-02-21 23:49:57 -0500

    Capitalized all title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,10 @@
     import ImagePreviewCard from "$lib/ImagePreviewCard.svelte";
 
     const images = [
-        { src: "../../Home_page_pics/wireframe.png", text: "UIUX", link: '/uiux' },
-        { src: "../../Home_page_pics/graphic_d.png", text: "Graphic Design", link: '/graphic_design' },
-        { src: "../../Home_page_pics/fitlogo.png", text: "Animation", link: '/animation_route' },
-        { src: "../../Home_page_pics/drawing.JPG", text: "Illustration", link: '/illustration' },
+        { src: "../../Home_page_pics/wireframe.png", text: "UIUX PROJECT", link: '/uiux' },
+        { src: "../../Home_page_pics/graphic_d.png", text: "GRAPHIC DESIGN", link: '/graphic_design' },
+        { src: "../../Home_page_pics/fitlogo.png", text: "ANIMATION", link: '/animation_route' },
+        { src: "../../Home_page_pics/drawing.JPG", text: "ILLUSTRATION", link: '/illustration' },
         // { src: "../../Home_page_pics/drawing.JPG", text: "Sketches", link: '/sketches_route' },
         // { src: "../../Home_page_pics/photography.JPG", text: "Photography", link: '/photography' },
     ];
@@ -17,7 +17,10 @@
             <ImagePreviewCard {...image}/>
         {/each}
     </div>
+    <div class="text">
+    </div>
 </html>
+
 
 <style>
     .join {
@@ -27,12 +30,19 @@
         gap: 2rem;
         padding: 2rem;
     }
+    .text {
+        font-size: 20px; 
+   }
     /* make the peft and right padding be 1rem when on mobile screens */
     @media (max-width: 640px) {
         .join {
-            padding-left: 1rem;
-            padding-right: 1rem;
+            padding-left: 0.5rem;
+            padding-right: 0.5rem;
         }
+        .text {
+        font-size: 14px; 
+   }
     }
+   
 </style>
 
